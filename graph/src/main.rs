684117ece99392f16d33d9fc18016ab132052a03
Rémy Greinhofer <remy.greinhofer@gmail.com>
2022-04-10 13:19:39 -0500

    Make a finer graph (#3)
    Renders a graph fine more tick line and a zoomed in view to make it
    easier to spot the values.
    
    The boundaries are dynamically generated, and the "secondary" as an
    increment of 0.25.

--- graph/src/main.rs
+++ graph/src/main.rs
@@ -9,20 +9,48 @@ use poloto::prelude::*;
 fn main() -> Result<(), Report> {
     // Load all the rates.
     let rates = load_rates();
+    let min_rate = rates
+        .iter()
+        .map(|r| r.rate)
+        .fold(f64::INFINITY, |a, b| a.min(b));
+    let max_rate = rates.iter().map(|r| r.rate).fold(0.0, |a: f64, b| a.max(b));
+    // dbg!(&max_rate);
+    let min_boundary = min_rate.floor() as i64 * 4;
+    let max_boundary = max_rate.ceil() as i64 * 4;
+    // dbg!(&min_boundary);
+    // dbg!(&max_boundary);
 
     let purchase_30 = get_term_data(&rates, 30);
     let purchase_20 = get_term_data(&rates, 20);
     let purchase_15 = get_term_data(&rates, 15);
-    dbg!(&purchase_30);
+    // dbg!(&purchase_30);
 
     let l1 = line("30 years", purchase_30);
     let l2 = line("20 years", purchase_20);
     let l3 = line("15 years", purchase_15);
 
-    let mut plotter = plots!(l1, l2, l3).build_with([], [0.0]).stage().plot(
-        "lower.com purchase rates",
-        "Date",
-        "Rate (%)",
+    let m = poloto::build::markers([], [min_rate.floor()]);
+    // let data = plots!(l1, l2, l3, m);
+    let data = poloto::data(plots!(l1, l2, l3, m));
+
+    let opt = poloto::render::render_opt_builder()
+        .with_tick_lines([true, true])
+        .build();
+    let (by, _) = poloto::ticks::bounds(&data, &opt);
+    let xtick_fmt = poloto::ticks::from_default(by);
+    let ytick_fmt =
+        poloto::ticks::from_iter((min_boundary..=max_boundary).map(|x| x as f64 * 0.25));
+
+    let plotter = poloto::plot_with(
+        data,
+        opt,
+        poloto::plot_fmt(
+            "lower.com purchase rates",
+            "Date",
+            "Rate (%)",
+            xtick_fmt,
+            ytick_fmt,
+        ),
     );
 
     // Write the graph to disk.
